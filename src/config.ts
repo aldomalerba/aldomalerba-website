c5ff1610639594250ee33eb694fd5aed8957d7b6
Aldo Malerba <aldomalerba.dev@gmail.com>
2023-05-21 20:59:39 +0200

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Aldo Malerba";
+export const SITE_DESCRIPTION =
+  "Portfolio e Blog di Aldo Malerba";
+export const TWITTER_HANDLE = "@";
+export const MY_NAME = "Aldo Malerba";
+
+// setup in astro.config.mjs
+const BASE_URL = new URL(import.meta.env.SITE);
+export const SITE_URL = BASE_URL.origin;
